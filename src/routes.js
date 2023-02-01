0f2c067dbd5a656a081d3822ed8cb53f01dcbdbb
Hatus Níwman <hatusn@gmail.com>
2023-02-01 17:09:30 -0300

    create server, routes, database

---
+++ src/routes.js
@@ -0,0 +1,63 @@
+import { randomUUID } from 'node:crypto';
+
+import { Database } from './database.js';
+import { buildRoutePath } from './utils/build-route-path.js';
+
+const database = new Database();
+
+export const routes = [
+  {
+    method: 'GET',
+    path: buildRoutePath('/tasks'),
+    handler: (req, res) => {
+      const { search } = req.query;
+
+      const tasks = database.select('tasks', {
+        description: search,
+        title: search,
+      });
+
+      return res.end(JSON.stringify(tasks));
+    },
+  },
+  {
+    method: 'POST',
+    path: buildRoutePath('/tasks'),
+    handler: (req, res) => {
+      const { description, title } = req.body;
+
+      const task = {
+        id: randomUUID(),
+        description,
+        title,
+      };
+
+      database.insert('tasks', task);
+
+      return res.writeHead(201).end();
+    },
+  },
+  {
+    method: 'PUT',
+    path: buildRoutePath('/tasks/:id'),
+    handler: (req, res) => {
+      const { id } = req.params;
+      const { name, email } = req.body;
+
+      database.update('tasks', id, { name, email });
+
+      return res.writeHead(204).end();
+    },
+  },
+  {
+    method: 'DELETE',
+    path: buildRoutePath('/tasks/:id'),
+    handler: (req, res) => {
+      const { id } = req.params;
+
+      database.delete('tasks', id);
+
+      return res.writeHead(204).end();
+    },
+  },
+];
